68e20ee9ac7f47fc4cde1a20ac8abb211b9ddf8f
Raj Vikas Tupe <rajvikastupe@NMAC-64.local>
2025-03-03 11:14:14 +0530

    compleated with the code-base setup

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Expense Tracker API");
+});
+
+app.use("/api/auth", authRoutes);
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
